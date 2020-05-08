71104467b0b58d4e84407b8afb6341489ef72905
zansibal <abrahamsson.richard@gmail.com>
2020-05-08 23:05:24 +0200

    Update __init__.py

--- trading_ig/__init__.py
+++ trading_ig/__init__.py
@@ -11,8 +11,8 @@ from __future__ import absolute_import, division, print_function
 
 
 from .version import (__author__, __copyright__, __credits__, __license__,
-                      __version__, __maintainer__, __email__, __status__,
-                      __url__)
+                      __version__, __maintainer__, __contributor__, __email__,
+                      __status__, __url__)
 
 from .rest import IGService
 from .stream import IGStreamService
