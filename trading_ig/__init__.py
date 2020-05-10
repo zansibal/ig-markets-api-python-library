417ae0275b0bab86d9da919791a4f7a7733cbfba
zansibal <abrahamsson.richard@gmail.com>
2020-05-10 09:48:22 +0200

    added contributor to list of getable items

--- trading_ig/__init__.py
+++ trading_ig/__init__.py
@@ -19,5 +19,5 @@ from .stream import IGStreamService
 
 __all__ = ['IGService', 'IGStreamService', '__author__',
            '__copyright__', '__credits__', '__license__',
-           '__version__', '__maintainer__', '__email__',
-           '__status__', '__url__']
+           '__version__', '__maintainer__', '__contributor__',
+           '__email__', '__status__', '__url__']
